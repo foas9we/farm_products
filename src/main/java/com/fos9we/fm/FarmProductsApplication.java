c9fcbc73d0685fbfed56f191e8684508e557033b
foas9we <2561082833@qq.com>
2020-04-03 22:05:07 +0800

    项目基本框架搭建1

---
+++ src/main/java/com/fos9we/fm/FarmProductsApplication.java
@@ -0,0 +1,13 @@
+package com.fos9we.fm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fos9we/fm/FarmProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fos9we.fm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
